558e18ebbcb94ec097e5e12ec3c6245c459815f1
francisco-1998 <francmarinc@gmail.com>
2021-01-31 14:03:37 -0500

    Cambiando el idioma de las fechas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
+//Modulos personalizados
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { PracticaModule } from './practica/practica.module';
+
+//Cambiar el locale de la app
+import  localeEs  from "@angular/common/locales/es-EC";
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
@@ -16,7 +23,9 @@ import { PracticaModule } from './practica/practica.module';
     SharedModule,
     PracticaModule
   ],
-  providers: [],
+  providers: [
+    {provide:LOCALE_ID, useValue:'es-EC' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
