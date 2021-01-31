ef6cbb96bc738eebfad59f64b50b76e3c814e274
francisco-1998 <francmarinc@gmail.com>
2021-01-31 12:51:13 -0500

    Agregando funcionalidad de navegacion en el menuBar

--- src/app/shared/menu-bar/menu-bar.component.ts
+++ src/app/shared/menu-bar/menu-bar.component.ts
@@ -17,28 +17,32 @@ export class MenuBarComponent implements OnInit {
 
   initMenuBar(){
     this.items = [
-      {
-          label: 'File',
-          items: [{
-                  label: 'New',
-                  icon: 'pi pi-fw pi-plus',
-                  items: [
-                      {label: 'Project'},
-                      {label: 'Other'},
-                  ]
-              },
-              {label: 'Open'},
-              {label: 'Quit'}
-          ]
-      },
-      {
-          label: 'Edit',
-          icon: 'pi pi-fw pi-pencil',
+        {
+          label: 'Pipes de Angular',
+          icon: 'pi pi-desktop',
           items: [
-              {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-              {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+              {
+                label: 'Textos y fechas',
+                icon: 'pi pi-align-left',
+                routerLink: '/',
+              },
+              {
+                label: 'Números',
+                icon: 'pi pi-dollar',
+                routerLink: 'numeros',
+              },
+              {
+                label: 'No comunes',
+                icon: 'pi pi-globe',
+                routerLink: 'no-comunes',
+              },
+
           ]
-      }
+        },
+        {
+          label: 'Pipes personalizados',
+          icon: 'pi pi-cog'
+        },
   ];
   }
 
