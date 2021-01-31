cce729fe0e82341df66bc56d1eecbafa2591a643
francisco-1998 <francmarinc@gmail.com>
2021-01-31 13:06:33 -0500

    Implementando Flexbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { PracticaModule } from './practica/practica.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     AppRoutingModule,
     BrowserModule,
-    SharedModule
+    SharedModule,
+    PracticaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
