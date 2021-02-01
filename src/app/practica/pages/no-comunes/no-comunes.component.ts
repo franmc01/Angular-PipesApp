8bd07a08cdf3d9500c528458d63ec6359988ff77
francisco-1998 <francmarinc@gmail.com>
2021-02-01 10:02:34 -0500

    Aprendiendo a usar el i18nSelect e i18nPlural Pipe

--- src/app/practica/pages/no-comunes/no-comunes.component.ts
+++ src/app/practica/pages/no-comunes/no-comunes.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
   styleUrls: ['./no-comunes.component.css']
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  //i18nSelect Pipe
+  persona = { nombre: 'Lucia', genero: 'F' }
+  mapping = { 'M':'invitarlo', 'F':'invitarla' }
 
-  ngOnInit(): void {
+  cambiarPersona(){
+    this.persona.nombre = 'Francisco';
+    this.persona.genero = 'M'
+  }
+
+
+  //i18nPlural Pipe
+  clientes:string[] = ['Juan', 'Maria', 'Pedro', 'Francisco'];
+  clientesMap = {
+    '=0': 'no tenemos clientes esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  eliminarCliente(){
+    this.clientes.shift();
   }
 
 }
